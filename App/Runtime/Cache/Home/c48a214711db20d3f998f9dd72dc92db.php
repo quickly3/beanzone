6b126501b8b5e7759fc0f58f54a327f1088411e5
Hongbin.Zhou <642915416@qq.com>
2014-12-18 15:25:12 +0800

    2014.12.18
    网站首页第一版

--- App/Runtime/Cache/Home/c48a214711db20d3f998f9dd72dc92db.php
+++ App/Runtime/Cache/Home/c48a214711db20d3f998f9dd72dc92db.php
@@ -7,12 +7,13 @@
 	<link type="text/css" rel="stylesheet" href="/beanzone/Public/vendor/font-awesome/css/font-awesome.css">
 	<link type="text/css" rel="stylesheet" href="/beanzone/Public/css/common/all.css">
 	
-	<link rel="stylesheet" href="<?php echo defaultCss();?>">
+    <link rel="stylesheet" href="<?php echo defaultCss();?>">
+
 	<script type="text/javascript" src="/beanzone/Public/vendor/jQuery-1.11.1.js"></script>
 	<script type="text/javascript" src="/beanzone/Public/vendor/bootstrap/js/bootstrap.min.js"></script>
 	<script type="text/javascript" src="/beanzone/Public/js/common/common.js"></script>
 	
-	<script type="text/javascript" src="<?php echo defaultJs();?>"></script>
+    <script type="text/javascript" src="<?php echo defaultJs();?>"></script>
 
 </head>
 	<header>
@@ -25,58 +26,9 @@
 		</div>
 	</header>
 <body>
-	<aside class="superside">
-		<a class="side-top"> <i class="glyphicon glyphicon-align-justify"></i>
-		</a>
-		<ul class="side-ul">
-			<li class="active">
-				<a href=""> <i class="glyphicon glyphicon-home"></i>
-				</a>
-			</li>
-			<li>
-				<a href="">
-					<i class="fa fa-clipboard"></i>
-				</a>
-			</li>
-			<li>
-				<a href="">
-					<i>C</i>
-				</a>
-			</li>
-			<li>
-				<a href="">
-					<i>D</i>
-				</a>
-			</li>
-			<li>
-				<a href="">
-					<i>E</i>
-				</a>
-			</li>
-			<li>
-				<a href="">
-					<i>F</i>
-				</a>
-			</li>
-			<li>
-				<a href="">
-					<i>G</i>
-				</a>
-			</li>
-			<li>
-				<a href="">
-					<i>H</i>
-				</a>
-			</li>
-		</ul>
-	</aside>
+
 	<nav>
 		<div class="nav-left">
-			<input type="text" class="form-control search" placeholder="...">
-			<i class="glyphicon glyphicon-search" id="searchIcon"></i>
-			<i class="go">Go</i>
-		</div>
-		<div class="nav-right">
 			<ul class="bean">
 				<li>
 					<img src="/beanzone/Public/images/common/bean.png" alt="">
@@ -87,39 +39,281 @@
 				</li>
 			</ul>
 		</div>
+		<div class="nav-right">
+			<input type="text" class="form-control search" placeholder="...">
+			<i class="glyphicon glyphicon-search" id="searchIcon"></i>
+		</div>
+
 	</nav>
 	<div class="container">
 	
-	<div id="myBlog">
-			<div class="blog">
-				<div class="b-l1">
-					<div class="b-label pre-cat">
-						<div class="pre-catinner">
-							<a href="">标签</a>
-						</div>
-						<div class="pre-catarrow"></div>
-					</div>
-					<div class="b-title">
-						<a href="">这是一个标题</a>
-					</div>
-					<div class="b-read ">
-						<span class="badge">2000 阅</span>
-					</div>
-					<div class="b-comment">
-						<span class="badge">20 评</span>
-					</div>
-				</div>
-				<div class="b-info">
-					<span class="b-date">9.18.2014</span>
-					|
-					<span class="author">周胖子</span>
-					|
-				</div>
-				<div class="b-content">
-					这是一个很长很长的故事非常非非常非非常非非常非非常非非常非非常非非常非非常非非常非非常非非常非非常非非常非非常非非常非非常非非常非非常非非常非非常非非常非非常非非常非非常非非常非非常非非常非非常非非常非非常非非常非非常非非常非非常非非常非非常非非常非非常非非常非非常非非常非非常非非常非非常非非常非非常非非常非非常非非常非非常非非常非非常非非常非非常非非常非非常非非常非非常非非常非非常非非常非非常非非常非非常非非常非非常非非常非非常非非常非非常非非常非非常非非常非非常非常的长
-				</div>
+    <div class="main row">
+        <div class="column1 col-lg-2">
+
+        </div>
+        <div class='column2 col-lg-6' id="myBlog">
+
+            <div class="blog row">
+                <div class="b-img col-lg-3">
+                    <img src="/beanzone/Public/images/common/default.jpg" alt="" class='img-thumbnail'>
+                </div>
+                <div class="col-lg-9">
+                    <div class="b-l1">
+                        <div class="b-label pre-cat">
+                            <div class="pre-catinner">
+                                <a href="">标签</a>
+                            </div>
+                            <div class="pre-catarrow"></div>
+                        </div>
+                        <div class="b-title">
+                            <a href="">这是一个标题</a>
+                        </div>
+                        <div class="b-read ">
+                            <span class="badge">2000 阅</span>
+                        </div>
+                        <div class="b-comment">
+                            <span class="badge">20 评</span>
+                        </div>
+                    </div>
+                    <div class="b-info">
+                        <span class="b-date">9.18.2014</span> |
+                        <span class="author">周胖子</span> |
+                    </div>
+                    <div class="b-content">
+                        这是一个很长很长的故事非常非非常非非常非非常非非常非非常非非常非非常非非常非非常非非常非非常非非常非非常非非常非非常非非常非非常非非常非非常非非常非非常非非常非非常非非常非非常非非常非非常非非常非非常非非常非非常非非常非非常非非常非非常非非常非非常非非常非非常非非常非非常非非常非非常非非常非非常非非常非非常非非常非非常非非常非非常非非常非非常非非常非非常非非常非非常非非常非非常非非常非非常非非常非非常非非常非非常非非常非非常非非常非非常非非常非非常非非常非非常非非常非常的长
+                    </div>
+                </div>
+            </div>
+            <div class="blog row">
+                <div class="b-img col-lg-3">
+                    <img src="/beanzone/Public/images/common/default.jpg" alt="" class='img-thumbnail'>
+                </div>
+                <div class="col-lg-9">
+                    <div class="b-l1">
+                        <div class="b-label pre-cat">
+                            <div class="pre-catinner">
+                                <a href="">标签</a>
+                            </div>
+                            <div class="pre-catarrow"></div>
+                        </div>
+                        <div class="b-title">
+                            <a href="">这是一个标题</a>
+                        </div>
+                        <div class="b-read ">
+                            <span class="badge">2000 阅</span>
+                        </div>
+                        <div class="b-comment">
+                            <span class="badge">20 评</span>
+                        </div>
+                    </div>
+                    <div class="b-info">
+                        <span class="b-date">9.18.2014</span> |
+                        <span class="author">周胖子</span> |
+                    </div>
+                    <div class="b-content">
+                        这是一个很长很长的故事非常非非常非非常非非常非非常非非常非非常非非常非非常非非常非非常非非常非非常非非常非非常非非常非非常非非常非非常非非常非非常非非常非非常非非常非非常非非常非非常非非常非非常非非常非非常非非常非非常非非常非非常非非常非非常非非常非非常非非常非非常非非常非非常非非常非非常非非常非非常非非常非非常非非常非非常非非常非非常非非常非非常非非常非非常非非常非非常非非常非非常非非常非非常非非常非非常非非常非非常非非常非非常非非常非非常非非常非非常非非常非非常非常的长
+                    </div>
+                </div>
+            </div>
+            <div class="blog row">
+                <div class="b-img col-lg-3">
+                    <img src="/beanzone/Public/images/common/default.jpg" alt="" class='img-thumbnail'>
+                </div>
+                <div class="col-lg-9">
+                    <div class="b-l1">
+                        <div class="b-label pre-cat">
+                            <div class="pre-catinner">
+                                <a href="">标签</a>
+                            </div>
+                            <div class="pre-catarrow"></div>
+                        </div>
+                        <div class="b-title">
+                            <a href="">这是一个标题</a>
+                        </div>
+                        <div class="b-read ">
+                            <span class="badge">2000 阅</span>
+                        </div>
+                        <div class="b-comment">
+                            <span class="badge">20 评</span>
+                        </div>
+                    </div>
+                    <div class="b-info">
+                        <span class="b-date">9.18.2014</span> |
+                        <span class="author">周胖子</span> |
+                    </div>
+                    <div class="b-content">
+                        这是一个很长很长的故事非常非非常非非常非非常非非常非非常非非常非非常非非常非非常非非常非非常非非常非非常非非常非非常非非常非非常非非常非非常非非常非非常非非常非非常非非常非非常非非常非非常非非常非非常非非常非非常非非常非非常非非常非非常非非常非非常非非常非非常非非常非非常非非常非非常非非常非非常非非常非非常非非常非非常非非常非非常非非常非非常非非常非非常非非常非非常非非常非非常非非常非非常非非常非非常非非常非非常非非常非非常非非常非非常非非常非非常非非常非非常非非常非常的长
+                    </div>
+                </div>
+            </div>
+            <div class="blog row">
+                <div class="b-img col-lg-3">
+                    <img src="/beanzone/Public/images/common/default.jpg" alt="" class='img-thumbnail'>
+                </div>
+                <div class="col-lg-9">
+                    <div class="b-l1">
+                        <div class="b-label pre-cat">
+                            <div class="pre-catinner">
+                                <a href="">标签</a>
+                            </div>
+                            <div class="pre-catarrow"></div>
+                        </div>
+                        <div class="b-title">
+                            <a href="">这是一个标题</a>
+                        </div>
+                        <div class="b-read ">
+                            <span class="badge">2000 阅</span>
+                        </div>
+                        <div class="b-comment">
+                            <span class="badge">20 评</span>
+                        </div>
+                    </div>
+                    <div class="b-info">
+                        <span class="b-date">9.18.2014</span> |
+                        <span class="author">周胖子</span> |
+                    </div>
+                    <div class="b-content">
+                        这是一个很长很长的故事非常非非常非非常非非常非非常非非常非非常非非常非非常非非常非非常非非常非非常非非常非非常非非常非非常非非常非非常非非常非非常非非常非非常非非常非非常非非常非非常非非常非非常非非常非非常非非常非非常非非常非非常非非常非非常非非常非非常非非常非非常非非常非非常非非常非非常非非常非非常非非常非非常非非常非非常非非常非非常非非常非非常非非常非非常非非常非非常非非常非非常非非常非非常非非常非非常非非常非非常非非常非非常非非常非非常非非常非非常非非常非非常非常的长
+                    </div>
+                </div>
+            </div>
+            <div class="blog row">
+                <div class="b-img col-lg-3">
+                    <img src="/beanzone/Public/images/common/default.jpg" alt="" class='img-thumbnail'>
+                </div>
+                <div class="col-lg-9">
+                    <div class="b-l1">
+                        <div class="b-label pre-cat">
+                            <div class="pre-catinner">
+                                <a href="">标签</a>
+                            </div>
+                            <div class="pre-catarrow"></div>
+                        </div>
+                        <div class="b-title">
+                            <a href="">这是一个标题</a>
+                        </div>
+                        <div class="b-read ">
+                            <span class="badge">2000 阅</span>
+                        </div>
+                        <div class="b-comment">
+                            <span class="badge">20 评</span>
+                        </div>
+                    </div>
+                    <div class="b-info">
+                        <span class="b-date">9.18.2014</span> |
+                        <span class="author">周胖子</span> |
+                    </div>
+                    <div class="b-content">
+                        这是一个很长很长的故事非常非非常非非常非非常非非常非非常非非常非非常非非常非非常非非常非非常非非常非非常非非常非非常非非常非非常非非常非非常非非常非非常非非常非非常非非常非非常非非常非非常非非常非非常非非常非非常非非常非非常非非常非非常非非常非非常非非常非非常非非常非非常非非常非非常非非常非非常非非常非非常非非常非非常非非常非非常非非常非非常非非常非非常非非常非非常非非常非非常非非常非非常非非常非非常非非常非非常非非常非非常非非常非非常非非常非非常非非常非非常非非常非常的长
+                    </div>
+                </div>
+            </div>
+        </div>
+        <div class="column3 col-lg-4">
+            <div class="message ">
+                <textarea placeholder='我轻轻的来..'></textarea>
+                <span class='b-btn b-btn-green'>留言</span>
+            </div>
+            <div class='dias'>
+                <div class='dia'>
+                    <span class='portrait'><a href=""><img src="/beanzone/Public/images/common/bean.png" alt="" class='img-thumbnail'></a></span>
+                    <span class='body'>
+						<span class='user'><a href="">@Superman</a>:</span>
+                    <span>阿什顿徕卡就是第六届奥斯卡的理解爱上了大家卡时间打卡机塑料袋爱上</span>
+                    <span class='time'>10分钟前</span>
+                    </span>
+                </div>
+                <div class='dia'>
+                    <span class='portrait'><a href=""><img src="/beanzone/Public/images/common/bean.png" alt="" class='img-thumbnail'></a></span>
+                    <span class='body'>
+					<span class='user'><a href="">@Superman</a>:</span>
+                    <span>阿什顿徕卡就是第六届奥斯卡的理解爱上了大家卡时间打卡机塑料袋爱上</span>
+                    <span class='time'>10分钟前</span>
+                    </span>
+                </div>
+                <div class='dia'>
+                    <span class='portrait'><a href=""><img src="/beanzone/Public/images/common/bean.png" alt="" class='img-thumbnail'></a></span>
+                    <span class='body'>
+					<span class='user'><a href="">@Superman</a>:</span>
+                    <span>阿什顿徕卡就是第六届奥斯卡的理解爱上了大家卡时间打卡机塑料袋爱上</span>
+                    <span class='time'>10分钟前</span>
+                    </span>
+                </div>
+                <div class='dia'>
+                    <span class='portrait'><a href=""><img src="/beanzone/Public/images/common/bean.png" alt="" class='img-thumbnail'></a></span>
+                    <span class='body'>
+					<span class='user'><a href="">@Superman</a>:</span>
+                    <span>阿什顿徕卡就是第六届奥斯卡的理解爱上了大家卡时间打卡机塑料袋爱上</span>
+                    <span class='time'>10分钟前</span>
+                    </span>
+                </div>
+            </div>
+			<div class='newly'>
+				<span class="col3-title">最新文章</span>
+				<span>
+					<ul>
+						<li>
+							<a href="">关于开水煮白菜的思考研究的同一场理论和量子力学</a>
+							<a href="">关于开水煮白菜的思考研究的同一场理论和量子力学</a>
+							<a href="">关于开水煮白菜的思考研究的同一场理论和量子力学</a>
+							<a href="">关于开水煮白菜的思考研究的同一场理论和量子力学</a>
+							<a href="">关于开水煮白菜的思考研究的同一场理论和量子力学</a>
+						</li>
+					</ul>
+				</span>
 			</div>
-	</div>
+			<div class="pigeonhole">
+				<span class="col3-title">文章归档</span>
+				<span>
+					<ul>
+						<li>
+							<a href="">2014年七月</a>
+							<a href="">2014年八月</a>
+							<a href="">2014年九月</a>
+							<a href="">2014年十月</a>
+							<a href="">2014年十一月</a>
+							<a href="">2014年十二月</a>
+						</li>
+					</ul>
+				</span>
+			</div>
+			<div class="category">
+				<span class="col3-title">分类</span>
+				<span>
+					<ul>
+						<li>
+							<a href="">HTML5</a>
+							<a href="">CSS3</a>
+							<a href="">Javascript</a>
+							<a href="">jQuery</a>
+							<a href="">Shell</a>
+							<a href="">PHP扩展</a>
+							<a href="">Oracle</a>
+							<a href="">Mysql</a>
+							<a href="">Informix</a>
+							<a href="">PHP</a>
+							<a href="">工作小品</a>
+							<a href="">收集</a>
+							<a href="">未分类</a>
+						</li>
+					</ul>					
+				</span>
+			</div>
+			<div class="other">
+				<span class="col3-title">更多功能</span>
+				<span>
+					<ul>
+						<li>
+							<a href="">登陆</a>
+							<a href="">关于Bean</a>
+						</li>
+					</ul>					
+				</span>
+			</div>
+        </div>
+    </div>
+
+
 
 	</div>
 
--- Public/js/Home/Index/index.js
+++ Public/js/Home/Index/index.js
@@ -1,16 +1,25 @@
+// ;(function(window,$){
+// 	var randomColor = function(elem){
+
+// 	}
+// })(window,$);
 $(function(){
 	$("#searchIcon").click(function(){
-		$(".search").animate({
-			marginLeft:"10"
-		},function(){
-			$("#searchIcon").fadeOut(function(){
-				$(".go").fadeIn();	
+		$obj = $({})
+			.queue('search',function(next){
+				$(".search").animate({marginLeft:"10"});
+				$("#searchIcon").animate({marginLeft:"10"});
+				next();
+			})
+			.queue('search',function(next){
+				$("#searchIcon").fadeOut();	
+				next();
+			})
+			.queue('search',function(){
+				$(".go").fadeIn();
 			});
-		}).focus();
-		$("#searchIcon").animate({
-			marginLeft:"10"
-		});
 	});
+
 	$(".search").dblclick(function(){
 		$(".search").animate({
 			marginLeft:"-210"
@@ -21,5 +30,15 @@ $(function(){
 		$("#searchIcon").animate({
 			marginLeft:"20"
 		});		
+	});
+
+	$('.category ul li a').each(function(){
+		var colorSet = ['#e8443a','#aa5096','#1CA1E2','#FFD302','#FFD302','#9B4C13','#8DC027','#FF0198','#4837cd','#33ac5b','#20c8a6'],
+			cLen,ranNum;
+
+		cLen = colorSet.length;
+		ranNum = Math.ceil(Math.random()*cLen) - 1;
+		
+		$(this).css({backgroundColor:colorSet[ranNum]});
 	});
 });
