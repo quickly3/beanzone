9acff00999f277e28cfb864c09d7104270eabb99
quickly3 <quickly3@sohu.com>
2015-03-04 14:33:26 +0800

    20150304
    20150304

--- App/Admin/Controller/IndexController.class.php
+++ App/Admin/Controller/IndexController.class.php
@@ -7,11 +7,31 @@ class IndexController extends RestController{
 		$this->display(defaultTpl());
 	}
 
+	public function logout(){
+		unset($_SESSION['user_name']);
+		unset($_SESSION['user_id']);
+	}
+
 	public function loginHandle(){
 		$name = I('user_name');
-		$pass = I('user_pass');
-		$_SESSION['user_name'] = $name;
-		$_SESSION['user_id'] = 1;
+		$pass = Md5('Bean'.I('user_pass'));
+
+
+		$sql = "select * from b_users where user_login='$name' and user_pass = '$pass'";
+		$M = M();
+		$res = $M->query($sql);
+
+		if($res){
+			$_SESSION['user_name'] = $name;
+			$_SESSION['user_id'] = 1;				
+		}
+
+		$ret = array(
+			"sta"=>'1',
+			"mes"=>'loginSuccess'
+			);
+		$this->ajaxReturn($ret);
+
 	}
 }
 
