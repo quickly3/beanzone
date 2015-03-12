f42c7373d42393bd8277d0e089cad79852b160f7
quickly3 <quickly3@sohu.com>
2015-03-12 18:10:25 +0800

    test2
    test2

--- App/Home/Controller/CommonController.class.php
+++ App/Home/Controller/CommonController.class.php
@@ -5,14 +5,15 @@ use Think\Controller;
 
 class CommonController extends Controller {
     public function imgUpload(){
-        import('Org.Bean.image');
-        $image = new \image("2.png", 1, "300", "500", "5.png");
+        import('Org.Bean.CImage');
+        
         $type =  array_pop(split('\.',$_FILES['files']['name']));
 
         $dest = C('IMAGE_HUB').'/tmp'.time().'.'.$type;
-
-        move_uploaded_file($_FILES['files']['tmp_name'],C('SERVER_PATH').$dest);
-
+        $newImg = C('SERVER_PATH').$dest;
+        move_uploaded_file($_FILES['files']['tmp_name'],$newImg);
+        $image = new \CImage();
+        $image->CreateThumbnail($newImg,170,120,$newImg);
         $res= array('sta'=>1,'mes'=>'success','img'=>$dest);
 
         $this->ajaxReturn($res);
